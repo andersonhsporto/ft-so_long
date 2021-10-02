c9c62da636c06507dfcf2356ea6a012241ea2fbf
Anderson Porto <andersonhigo2@gmail.com>
2021-10-02 01:11:58 -0300

    Create so_long.h

---
+++ includes/so_long.h
@@ -0,0 +1,23 @@
+#ifndef SO_LONG_H
+# define SO_LONG_H
+
+# include "../mlx/mlx.h"
+
+# include "../libft/libft.h"
+
+# include <stdio.h>
+
+# include <X11/X.h>
+
+# include <math.h>
+
+#include <string.h>
+#include <stdlib.h>
+
+typedef struct s_game
+{
+	void	*mlx;
+	void	*window_pointer;
+}	t_game;
+
+#endif
