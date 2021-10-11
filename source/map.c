e94046407b62549dcf05a4ca47cc5578c7d0928a
Anderson Porto <anderson.higo2@gmail.com>
2021-10-11 00:15:58 -0300

    Update map.c

--- source/map.c
+++ source/map.c
@@ -12,9 +12,9 @@ void	map_maker(t_game	*game)
 		while (game->plot.map[y][x])
 		{
 			if (game->plot.map[y][x] == '1') //alterar [] []
-			mlx_put_image_to_window(game->mlx_pointer, game->window_pointer, game->floor.ptr,  (50 * x), (50 * y));
+			mlx_put_image_to_window(game->mlx_pointer, game->window_pointer, game->wall.ptr,  (26 * x), (32 * y));
 			else
-			mlx_put_image_to_window(game->mlx_pointer, game->window_pointer, game->wall.ptr,  (50 * x), (50 * y));
+			mlx_put_image_to_window(game->mlx_pointer, game->window_pointer, game->floor.ptr,  (26 * x), (32 * y));
 			x++;
 		}
 		y++;
@@ -22,23 +22,46 @@ void	map_maker(t_game	*game)
 	return ;
 }
 
+int	len_map(char **map)
+{
+	int i;
+	int j;
+	int k;
+
+	i = 0;
+	j = 0;
+	k = ft_strlen(map[0]);
+	while (map[i] != 0)
+	{
+		j = ft_strlen(map[i]);
+		if (k != j)
+			return (0);
+		i++;
+	}
+	return(k);
+}
+
 void	init_map(t_game *game, char *path)
 {
 	int	fd;
+	int	i;
 
 	fd = open(path, O_RDONLY);
 	if (fd < 0)
 		return ; //verificar depois
 	game->plot.temp = ft_strdup("");
+	i = 0;
 	while ((game->plot.line = get_next_line(fd)))
 	{
 		if (!game->plot.line)
 			break ;
 		game->plot.temp = gnl_strjoin(game->plot.temp, game->plot.line);
 		free(game->plot.line);
+		i++;
 	}
 	game->plot.map = ft_split(game->plot.temp, '\n');
-	free(game->plot.temp);
+	game->plot.height = i;
+	game->plot.length = len_map(game->plot.map);
 	close(fd);
 	return ;
 }
