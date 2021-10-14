98a01e6e19ba77626cd0354212e6f015b73d2a8f
Anderson Porto <anderson.higo2@gmail.com>
2021-10-14 02:13:38 -0300

    Update map.c

--- source/map.c
+++ source/map.c
@@ -6,32 +6,11 @@
 /*   By: anhigo-s <anhigo-s@student.42sp.org.br     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/11 21:29:12 by anhigo-s          #+#    #+#             */
-/*   Updated: 2021/10/12 00:25:39 by anhigo-s         ###   ########.fr       */
+/*   Updated: 2021/10/14 02:10:03 by anhigo-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/so_long.h"
-
-static void	check_map_maker(t_game *game, int y, int x)
-{
-	if (game->plot.map[y][x] == '1')
-		mlx_put_image_to_window(game->mlx_pointer, game->window_pointer,
-			game->wall.ptr, (26 * x), (32 * y));
-	else if (game->plot.map[y][x] == 'C')
-	{
-		if ((game->character.x / 26) == x && (game->character.y / 32) == y)
-		{
-			game->plot.map[y][x] = '0';
-			mlx_put_image_to_window(game->mlx_pointer, game->window_pointer,
-				game->collect.ptr, (26 * x), (32 * y));
-		}
-		mlx_put_image_to_window(game->mlx_pointer, game->window_pointer,
-			game->collect.ptr, (26 * x), (32 * y));
-	}
-	else
-		mlx_put_image_to_window(game->mlx_pointer, game->window_pointer,
-			game->floor.ptr, (26 * x), (32 * y));
-}
 
 void	player_init(t_game	*game)
 {
@@ -46,10 +25,10 @@ void	player_init(t_game	*game)
 		{
 			if (game->plot.map[y][x] == 'P')
 			{
-				game->character.x = (26 * x);
+				game->character.x = (32 * x);
 				game->character.y = (32 * y);
 				mlx_put_image_to_window(game->mlx_pointer, game->window_pointer,
-					game->character.ptr, (26 * x), (32 * y));
+					game->character.ptr, (32 * x), (32 * y));
 				return ;
 			}
 			x++;
@@ -59,7 +38,71 @@ void	player_init(t_game	*game)
 	return ;
 }
 
-void	map_maker(t_game	*game)
+static void	check_map_maker(t_game *game, int y, int x)
+{
+	if (game->plot.map[y][x] == '1')
+		mlx_put_image_to_window(game->mlx_pointer, game->window_pointer,
+			game->wall.ptr, (32 * x), (32 * y));
+	else if (game->plot.map[y][x] == 'C')
+	{
+		if ((game->character.x / 32) == x && (game->character.y / 32) == y)
+		{
+			game->plot.map[y][x] = '0';
+			game->i.collectible--;
+			mlx_put_image_to_window(game->mlx_pointer, game->window_pointer,
+				game->collect.ptr, (32 * x), (32 * y));
+		}
+		mlx_put_image_to_window(game->mlx_pointer, game->window_pointer,
+			game->collect.ptr, (32 * x), (32 * y));
+	}
+	else
+		mlx_put_image_to_window(game->mlx_pointer, game->window_pointer,
+			game->floor.ptr, (32 * x), (32 * y));
+}
+
+void	map_check_two(t_game *game, int y, int x)
+{
+	if (game->plot.map[y][x] == 'C')
+		game->i.collectible++;
+	else if (game->plot.map[y][x] == 'E')
+		game->i.map_exit++;
+	else if (game->plot.map[y][x] == 'P')
+		game->i.starting++;
+	if (game->i.collectible > 0 || game->i.map_exit > 0 || game->i.starting == 1)
+		check_map_maker(game, y, x);
+	else
+	{
+		printf("Error\nInvalid Map\n");
+		exit(1);
+	}
+	return ;
+}
+
+void	map_check_one(t_game *game, int y, int x)
+{
+	if ((game->plot.map[game->plot.height - 1][x] != '1') ||
+			(game->plot.map[0][x] != '1') || (game->plot.map[y]
+			[game->plot.length - 1] != '1') || (game->plot.map[y]
+			[0] != '1'))
+	{
+		printf("Error\nNot surrounded by trees!\n");
+		exit(1);
+	}
+	if (game->plot.height == game->plot.length)
+	{
+		printf("Error\nMap is not rectangular!\n");
+		exit(1);
+	}
+	if (!(ft_strchr("01CEP", game->plot.map[y][x])))
+	{
+		printf("Error\nInvalid Map");
+		exit(1);
+	}
+	map_check_two(game, y, x);
+	return ;
+}
+
+void	map_maker(t_game *game)
 {
 	int	y;
 	int	x;
@@ -70,7 +113,7 @@ void	map_maker(t_game	*game)
 		x = 0;
 		while (game->plot.map[y][x])
 		{
-			check_map_maker(game, y, x);
+			map_check_one(game, y, x);
 			x++;
 		}
 		y++;
@@ -97,6 +140,13 @@ int	len_map(char **map)
 	return (k);
 }
 
+void	exit_game(int fd)
+{
+	printf("Error\nCan't find file!\n");
+	close(fd);
+	exit(1);
+}
+
 void	init_map(t_game *game, char *path)
 {
 	int	fd;
@@ -104,7 +154,7 @@ void	init_map(t_game *game, char *path)
 
 	fd = open(path, O_RDONLY);
 	if (fd < 0)
-		return ; //verificar depois
+		exit_game(fd);
 	game->plot.temp = ft_strdup("");
 	i = 0;
 	while (fd)
