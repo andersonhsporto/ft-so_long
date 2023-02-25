b8615c9d198259358698072212754c491ec2718d
Anderson Porto <anderson.higo2@gmail.com>
2023-02-25 02:30:27 -0300

    refactor: using init_hook function

--- source/so_long.c
+++ source/so_long.c
@@ -6,7 +6,7 @@
 /*   By: anhigo-s <anhigo-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/16 20:11:53 by anhigo-s          #+#    #+#             */
-/*   Updated: 2023/02/25 02:07:36 by anhigo-s         ###   ########.fr       */
+/*   Updated: 2023/02/25 02:30:00 by anhigo-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,18 @@
 
 static void	init_game(t_game *game, char *path);
 static bool	check_arg(const char *argv);
+static void	init_hook(t_game *game, int event, \
+int mask, int (*f)());
 
 int	main(int argc, char *argv[])
 {
 	t_game	game;
 
 	if (argc == 2 && (check_arg(argv[1])))
-	{
 		init_game(&game, argv[1]);
-	}
-	if ((argc == 2 && !(check_arg(argv[1]))))
+	else if ((argc == 2 && !(check_arg(argv[1]))))
 		endgame("Can't open file. The format is not supported!", &game, error);
-	if (argc > 2)
+	else if (argc > 2)
 		endgame("Can't open multiple files!", &game, error);
 	else
 		endgame("Please specify file name!", &game, error);
@@ -35,15 +35,12 @@ int	main(int argc, char *argv[])
 static void	init_game(t_game *game, char *path)
 {
 	init_map(game, path);
-	init_so_long(game);
+	init_window(game);
 	init_images(game);
-	map_maker(game);
-	mlx_hook(game->window_pointer, \
-		KEY_RELEASE, KEY_RELEASE_MASK, key_check, game);
-	mlx_hook(game->window_pointer, \
-		DESTROY_NOTIFY, NO_EVENT_MASK, red_cross, game);
-	mlx_hook(game->window_pointer, \
-		12, EXPOSURE_MASK, mini_maker, game);
+	render_map(game);
+	init_hook(game, KEY_RELEASE, KEY_RELEASE_MASK, key_check);
+	init_hook(game, DESTROY_NOTIFY, NO_EVENT_MASK, red_cross);
+	init_hook(game, EXPOSE, EXPOSURE_MASK, mini_maker);
 	mlx_loop(game->mlx_pointer);
 }
 
@@ -61,4 +58,15 @@ static bool	check_arg(const char *argv)
 			return (true);
 	}
 	return (false);
+}
+
+static void	init_hook(t_game *game, int event, \
+int mask, int (*f)())
+{
+	mlx_hook(
+		game->window_pointer,
+		event,
+		mask,
+		f,
+		game);
 }
